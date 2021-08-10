d859ec2c340acd55ce173dbe3bd8b240535328c1
knshika <poojakumari17032001@gmail.com>
2021-08-11 02:33:18 +0530

    learned why nextjs, routing, styling and prerendering

---
+++ nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,27 @@
+// export default function FirstPost() {
+//     return <h1>First Post</h1>
+//   }
+
+import Head from "next/head";
+import Link from "next/link";
+
+import React from 'react'
+import Layout from "../../components/layout";
+
+const FirstPost = () => {
+    return (
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <h1>First Post</h1>
+            <h2>
+                <Link href="/">
+                    <a>Back to home</a>
+                </Link>
+            </h2>
+        </Layout>
+    )
+}
+
+export default FirstPost
